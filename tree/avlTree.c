0061969b90afd6e116efb3692fc54c19fee26bf5
Lin Hai <clearlylin@outlook.com>
2016-07-01 14:47:35 +0800

    tree/avlTree.c: delete node

--- tree/avlTree.c
+++ tree/avlTree.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-//插入新节点后，更新左右子树差值
 static int updateDiff(AVLNode* node)
 {
 	if (node) {
@@ -17,7 +16,6 @@ static int updateDiff(AVLNode* node)
 	return 0;
 }
 
-//插入节点后，检测是否不平衡
 static AVLNode* checkIsBalance(AVLNode* root)
 {
 	if (!root)
@@ -67,30 +65,6 @@ static void rotateRight(AVLNode* node)
 	left->right = node;
 }
 
-//static void rotateLeft2(AVLNode* node)
-//{
-//	AVLNode* p = node->parent;
-//	node->parent = p->parent;
-//	node->parent->left = node;
-//	p->right = node->left;
-//	if (p->right)
-//		p->right->parent = p;
-//	node->left = p;
-//	p->parent = node;
-//}
-////不平衡点的孙子旋转
-//static void rotateRight2(AVLNode* node)
-//{
-//	AVLNode* p = node->parent;
-//	node->parent = p->parent;
-//	node->parent->right = node;
-//	p->left = node->right;
-//	if (p->left)
-//		p->left->parent = p;
-//	node->right = p;
-//	p->parent = node;
-//}
-
 AVLNode* createAVLNode(int key, void* value)
 {
 	AVLNode* node = (AVLNode*)malloc(sizeof(AVLNode));
@@ -102,6 +76,33 @@ AVLNode* createAVLNode(int key, void* value)
 	node->parent = NULL;
 	node->value = value;
 	return node;
+}
+
+static void adjustTree(AVLNode** root)
+{
+	updateDiff(*root);
+	AVLNode*  balance = checkIsBalance(*root);
+	if (balance) {
+		if (balance->diff > 0) {
+			if (balance->left->diff > 0)
+				rotateRight(balance);
+			else {
+				rotateLeft(balance->left);
+				rotateRight(balance);
+			}
+		}
+		else {
+			if (balance->right->diff  > 0) {
+				rotateRight(balance->right);
+				rotateLeft(balance);
+			}
+			else
+				rotateLeft(balance);
+		}
+		if (balance == *root)
+			*root = balance->parent;
+		updateDiff(*root);
+	}
 }
 
 void insertAVLNode(AVLNode** root, AVLNode* node)
@@ -120,33 +121,11 @@ void insertAVLNode(AVLNode** root, AVLNode* node)
 	}
 	if (q->key > node->key)
 		q->left = node;
-	else 
+	else
 		q->right = node;
-	
+
 	node->parent = q;
-	updateDiff(*root);
-	AVLNode*  balance = checkIsBalance(*root);
-	if (balance) {
-		if (balance->diff > 0) {
-			if (balance->left->diff > 0)
-				rotateRight(balance);
-			else { 
-				rotateLeft(balance->left);
-				rotateRight(balance);
-			}
-		}
-		else {
-			if (balance->right->diff  > 0) { 
-				rotateRight(balance->right);
-				rotateLeft(balance);
-			}
-			else
-				rotateLeft(balance);
-		}
-		if (balance == *root)
-			*root = balance->parent;
-		updateDiff(*root);
-	}
+	adjustTree(root);
 }
 
 AVLNode* findAVLNode(AVLNode* root, int key)
@@ -162,12 +141,13 @@ AVLNode* findAVLNode(AVLNode* root, int key)
 	return NULL;
 }
 
-void deleteKey(AVLNode* root, int key)
+void deleteKey(AVLNode** root, int key)
 {
-	AVLNode* node = findAVLNode(root, key);
+	AVLNode* node = findAVLNode(*root, key);
 	if (node) {
+		AVLNode* left = NULL;
 		if (node->left && node->right) {
-			AVLNode* left = node->right;
+			left = node->right;
 			while (left->left)
 				left = left->left;
 			if (left != node->right) {
@@ -178,34 +158,24 @@ void deleteKey(AVLNode* root, int key)
 			left->parent = node->parent;
 			left->left = node->left;
 			left->left->parent = left;
+		}
+		else if (node->left) {
+			left = node->left;
+			left->parent = node->parent;
+		}
+		else if (node->right) {
+			left = node->right;
+			left->parent = node->parent;
+		}
+		if (node->parent) {
 			if (node->parent->left == node)
 				node->parent->left = left;
 			else
 				node->parent->right = left;
-			free(node);
 		}
-		else if (node->left) {
-			node->left->parent = node->parent;
-			if (node->parent->left == node)
-				node->parent->left = node->left;
-			else
-				node->parent->right = node->left;
-			free(node);
-		}
-		else if (node->right) {
-			node->right->parent = node->parent;
-			if (node->parent->left == node)
-				node->parent->left = node->right;
-			else
-				node->parent->right = node->right;
-			free(node);
-		}
-		else {
-			if (node->parent->left == node)
-				node->parent->left = NULL;
-			else
-				node->parent->right = NULL;
-		}
+		if (*root == node)
+			*root = left ? left : NULL;
+		adjustTree(root);
 	}
 }
 
